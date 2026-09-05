2762d922ab69dffec63d87bbfe05db4277809e87
ZaytsevSV <serge2012@yandex.ru>
2026-09-05 13:41:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,59 +3,64 @@ class Student:
         self.name = name
         self.surname = surname
         self.gender = gender
-        self.finished_courses = []
-        self.courses_in_progress = []
-        self.grades = {}
-        self.lecturer_grades = {}
+        self.finished_courses =
+        self.courses_in_progress =
+        self.grades = {}  # Оценки студента: курс -> список оценок 
 
     def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
-            if grade > 10:
-                return 'Ошибка: оценка не может быть больше 10'
-
-            if course in lecturer.grades:
-                lecturer.grades[course].append(grade)
-            else:
-                lecturer.grades[course] = [grade]
+        # Проверяем, что lecturer действительно экземпляр Lecturer
+        if not isinstance(lecturer, Lecturer):
+            return 'Ошибка: первый аргумент должен быть объектом Lecturer'
+        # Проверяем, что курс есть в списках курсов студента и лектора
+        if course not in self.courses_in_progress:
+            return 'Ошибка: курс не в списке курсов студента'
+        if course not in lecturer.courses_attached:
+            return 'Ошибка: курс не в списке курсов лектора'
+        # Проверяем корректность оценки
+        if not (0 <= grade <= 10):
+            return 'Ошибка: оценка должна быть от 0 до 10'
+        # Добавляем оценку
+        if course in lecturer.grades:
+            lecturer.grades[course].append(grade)
         else:
-            return 'Ошибка'
+            lecturer.grades[course] = [grade]
+        return f'Оценка {grade} добавлена'
+
+    def get_average_grade(self):
+        all_grades = [grade for grades in self.grades.values() for grade in grades]
+        return sum(all_grades) / len(all_grades) if all_grades else 0
 
     def __str__(self):
         avg_grade = self.get_average_grade()
         courses_in_progress = ', '.join(self.courses_in_progress)
         finished_courses = ', '.join(self.finished_courses)
-        return (f"Имя: {self.name}\nФамилия: {self.surname}\n"
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
                 f"Средняя оценка за домашние задания: {avg_grade:.1f}\n"
                 f"Курсы в процессе изучения: {courses_in_progress}\n"
                 f"Завершенные курсы: {finished_courses}")
-
-    def get_average_grade(self):
-        all_grades = [grade for grades in self.grades.values() for grade in grades]
-        return sum(all_grades) / len(all_grades) if all_grades else 0
 
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.courses_attached = []
+        self.courses_attached =  # Курсы, которые этот ментор ведёт 
 
     def __str__(self):
-        return f"Имя: {self.name}\nФамилия: {self.surname}"
+        return f"Имя: {self.name}\пФамилия: {self.surname}"
 
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades = {}
-
-    def get_average_grade(self):
-        all_grades = [grade for grades in self.grades.values() for grade in grades]
-        return sum(all_grades) / len(all_grades) if all_grades else 0
+        self.grades = {}  # Оценки студентов: курс -> список оценок
 
     def __str__(self):
         avg_grade = self.get_average_grade()
-        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_grade:.1f}"
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {avg_grade:.1f}")
 
 
 class Reviewer(Mentor):
@@ -65,32 +70,26 @@ class Reviewer(Mentor):
                 student.grades[course].append(grade)
             else:
                 student.grades[course] = [grade]
+            return f'Оценка {grade} добавлена'
         else:
-            return 'Ошибка'
+            return 'Ошибка: некорректные данные'
 
     def __str__(self):
-        return f"Имя: {self.name}\nФамилия: {self.surname}"
+        return f"Имя: {self.name}\пФамилия: {self.surname}"
 
+
+# Пример использования
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-best_student.finished_courses += ['Git']
+best_student.courses_in_progress = ['Python']
+best_student.finished_courses = ['Git']
 
 cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_mentor.courses_attached = ['Python']
 
 cool_lecturer = Lecturer('John', 'Smith')
-cool_lecturer.courses_attached += ['Python']
+cool_lecturer.courses_attached = ['Python']
 
-# Оценка студенту
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 9)
-cool_mentor.rate_hw(best_student, 'Python', 8)
-
-# Оценка лектору
-best_student.rate_lecturer(cool_lecturer, 'Python', 9)
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
-
-# Вывод информации
-print(best_student)
-print(cool_mentor)
-print(cool_lecturer)
+# Выставление оценок
+print(best_student.rate_lecturer(cool_lecturer, 'Python', 9))  # Оценка добавлена
+print(best_student.rate_lecturer(cool_lecturer, 'Python', 10))  # Оценка добавлена
+print(best_student.rate_lecturer(cool_lecturer, 'Java', 8))  # Ошибка: курс не найден
